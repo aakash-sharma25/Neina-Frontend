cd2118e8c40d11cfcaaebcef31cb13e358418db6
aakash-sharma25 <as024harma@gmail.com>
2025-01-03 17:40:45 +0530

    added components

---
+++ app/_components/DisplayBooking.js
@@ -0,0 +1,68 @@
+"use client";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import axios from "axios";
+
+const DisplayBooking = ({ bookings, removeBooking, loading }) => {
+  const deleteBooking = async (bookingId) => {
+    try {
+      const { data } = await axios.delete(
+        `http://localhost:5000/api/booking/${bookingId}`
+      );
+
+      if (data.success) {
+        alert("Booking deleted successfully!");
+        removeBooking(bookingId);
+      } else {
+        alert("Failed to delete booking:", data.message);
+      }
+    } catch (error) {
+      alert("error");
+      console.error("Error deleting booking:", error);
+    }
+  };
+
+  return (
+    <Box sx={{ padding: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        All Bookings
+      </Typography>
+
+      {loading && <CircularProgress />}
+
+      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 2, mt: 2 }}>
+        {bookings?.map((booking) => (
+          <Card key={booking._id} sx={{ width: 300 }}>
+            <CardContent>
+              <Typography variant="h6">Name: {booking.name}</Typography>
+              <Typography>Date: {booking.date}</Typography>
+              <Typography>Time: {booking.time}</Typography>
+              <Typography>Guests: {booking.guest}</Typography>
+              <Typography>Contact: {booking.contact}</Typography>
+            </CardContent>
+            <Box
+              sx={{ display: "flex", justifyContent: "space-between", p: 1 }}
+            >
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => deleteBooking(booking._id)}
+              >
+                Delete
+              </Button>
+            </Box>
+          </Card>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default DisplayBooking;
